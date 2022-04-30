5e2b3e70c031c16bae392451ab13c49cecb835b7
Abdulnaser Sheikh <67299192+abdulnsheikh@users.noreply.github.com>
2022-04-30 01:29:26 -0500

    s = s.lower()
    We do not need to call .lower() multiple times. We can just call it one time.

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,15 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
         l, r = 0, len(s) - 1
+        
+        # Sat April 29th, 2022
+        s = s.lower()
+        
         while l < r:
+            
             while l < r and not self.alphanum(s[l]): 
                 l += 1
+                
             while l < r and not self.alphanum(s[r]): 
                 r -= 1
-            if s[l].lower() != s[r].lower(): 
+                
+            if s[l] != s[r]: 
                 return False
+            
             l += 1
             r -= 1
+            
         return True
     
     # Could write own alpha-numeric function
