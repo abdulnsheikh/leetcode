76645031aef299b2196005aac1314a58f8fc7711
Abdulnaser Sheikh <67299192+abdulnsheikh@users.noreply.github.com>
2022-05-30 23:23:19 -0500

    res -> maxProfit
    updated the variable names

--- 121-Best-Time-To-Buy-and-Sell-Stock.py
+++ 121-Best-Time-To-Buy-and-Sell-Stock.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        maxProfit = 0
         
         l = 0
         for r in range(1, len(prices)):
+            
             if prices[r] < prices[l]:
                 l = r
-            res = max(res, prices[r] - prices[l])
-        return res
+                
+            maxProfit = max(maxProfit, prices[r] - prices[l])
+            
+        return maxProfit
